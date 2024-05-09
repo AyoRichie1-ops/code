030013be29572ffd988a842a288ae4e1fc320fd9
AyoRichie1-ops <onanusiabdullateef@gmail.com>
2024-05-09 09:20:08 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import Homepage from './pages/Homepage'
+import Contact from './pages/Contact'
+
+function App() {
+
+
+  return (
+    <div>
+      {/* <Homepage /> */}
+      <Contact />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import image from '../assets/image.png'
+import '../styles/Homepage.css'
+import { GoArrowRight } from "react-icons/go";
+import { Link } from 'react-router-dom';
+
+const Homepage = () => {
+    return (
+        <div className='homebody'>
+            <h4 className='homeh4'>
+                A Venture studio with a ‘unique’ twist
+            </h4>
+            <p className='homep'>
+                Explore the 24 apps with unique solutions and their metrics
+            </p>
+            <img className='image' src={image} alt="" />
+            <a className='text-decoration-none' href='#'>
+                <button className='homebtn'>
+                    <span className='textbtn'>Our Portfolio</span> <GoArrowRight className='iconbtn' />
+                </button>
+            </a>
+            <h5 className='homeh5'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </h5>
+        </div>
+    )
+}
+
+export default Homepage
